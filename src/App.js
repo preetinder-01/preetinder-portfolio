1da6532c215b307b3f1a2dd2acf9e19b6871803b
preetinder singh <preetindersinghbajaj@gmail.com>
2022-08-23 14:05:47 +0530

    latest chnages

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ import {useNavigate} from 'react-router'
 
 function App() {
   const history = useNavigate();
-  history(0)
+  const refreshPage = () => {
+    history(0);
+}
   return (
     <>
       <Routes>
-        <Route path="/" forceRefresh={true} element={<Layout />} >
-          <Route index forceRefresh={true} element={<Home />} />
-           <Route forceRefresh={true} path="about" element={<About />} />
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+           <Route path="about" element={<About />} />
            <Route path="/contact" element={<Contact />} />
            <Route path="/portfolio" element={<Portfolio />} />
            {/* <Route path="/dashboard" element={<Dashboard />} /> */}
