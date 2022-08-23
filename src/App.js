21199a9219beab5c8326dae9818f9e0d8f6c49a4
preetinder singh <preetindersinghbajaj@gmail.com>
2022-08-23 13:58:24 +0530

    final

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} >
+        <Route path="/" forceRefresh={true} element={<Layout />} >
           <Route index element={<Home />} />
            <Route path="about" element={<About />} />
            <Route path="/contact" element={<Contact />} />
