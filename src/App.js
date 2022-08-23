7abb503892133e4fbfc9a2888908b6269e2a0d0a
preetinder singh <preetindersinghbajaj@gmail.com>
2022-08-23 13:52:31 +0530

    added reload router settings

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import Layout from './components/Layout'
 import Portfolio from './components/Portfolio'
 // import Dashboard from './components/Dashboard'
 import './App.scss'
+import {useNavigate} from 'react-router'
 
 function App() {
+  const history = useNavigate();
+  const refreshPage = () => {
+    history(0);
+}
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />} >
           <Route index element={<Home />} />
            <Route path="about" element={<About />} />
            <Route path="/contact" element={<Contact />} />
